09006854d966bd1bcd554fba2c27b37255500a30
Andrés Carreño <andrebrayan74@gmail.com>
2022-01-31 19:25:04 -0500

    se agregaron la base del proyecto

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="font-display">
+      <h1>Hola</h1>
+    </div>
+  )
+}
+
+export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,69 +1,68 @@
-const Color = require("color");
+const Color = require('color');
 
 const lighten = (color, value) => Color(color).lighten(value).hex().toString();
 const darken = (color, value) => Color(color).darken(value).hex().toString();
 
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/containers/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/containers/**/*.{js,ts,jsx,tsx}'
   ],
-  mode: process.env.NODE_ENV ? "jit" : undefined,
+  mode: process.env.NODE_ENV ? 'jit' : undefined,
   theme: {
     extend: {
       container: {
         center: true,
         padding: {
-          DEFAULT: "1rem",
-          sm: "4rem",
-          lg: "6rem",
-          xl: "8rem",
-          "2xl": "10rem",
-        },
+          DEFAULT: '1rem',
+          sm: '4rem',
+          lg: '6rem',
+          xl: '8rem',
+          '2xl': '10rem'
+        }
       },
       colors: {
-        black: "#010101",
-        white: "#fffffe",
-        "eerie-black": {
-          light: lighten("#16161a", 0.08),
-          DEFAULT: "#16161a",
-          dark: darken("#16161a", 0.08),
+        black: '#010101',
+        white: '#fffffe',
+        'red-salsa': {
+          light: lighten('#EE4950', 0.08),
+          DEFAULT: '#EE4950',
+          dark: darken('#EE4950', 0.08)
         },
-        "raisin-black": {
-          light: lighten("#242629", 0.08),
-          DEFAULT: "#242629",
-          dark: darken("#242629", 0.08),
+        'eerie-black': {
+          light: lighten('#16161a', 0.08),
+          DEFAULT: '#16161a',
+          dark: darken('#16161a', 0.08)
         },
-        "medium-state-blue": {
-          light: lighten("#7f5af0", 0.08),
-          DEFAULT: "#7f5af0",
-          dark: darken("#7f5af0", 0.08),
+        'raisin-black': {
+          light: lighten('#242629', 0.08),
+          DEFAULT: '#242629',
+          dark: darken('#242629', 0.08)
         },
-        "cadet-grey": {
-          light: lighten("#94a1b2", 0.08),
-          DEFAULT: "#94a1b2",
-          dark: darken("#94a1b2", 0.08),
+        'spanish-gray': {
+          light: lighten('#959D99', 0.08),
+          DEFAULT: '#959D99',
+          dark: darken('#959D99', 0.08)
         },
-        "sonic-silver": {
-          light: lighten("#72757e", 0.08),
-          DEFAULT: "#72757e",
-          dark: darken("#72757e", 0.08),
+        'sonic-silver': {
+          light: lighten('#72757e', 0.08),
+          DEFAULT: '#72757e',
+          dark: darken('#72757e', 0.08)
         },
-        "medium-sea-green": {
-          light: lighten("#2cb67d", 0.08),
-          DEFAULT: "#2cb67d",
-          dark: darken("#2cb67d", 0.08),
-        },
+        'sonic-silver': {
+          light: lighten('#2E3A59', 0.08),
+          DEFAULT: '#2E3A59',
+          dark: darken('#2E3A59', 0.08)
+        }
       },
       fontFamily: {
-        display: ["Fira Code", "monospace"],
-        body: ["Merriweather Sans", "sans-serif"],
-      },
-    },
+        display: ['Inter', 'monospace']
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 };
