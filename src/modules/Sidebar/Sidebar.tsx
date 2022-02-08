d6c73fde13f074e3970521518f925b8b78743c54
Andrés Carreño <andrebrayan74@gmail.com>
2022-02-07 22:12:11 -0500

    chore: add react icons library and first navigation items

--- src/modules/Sidebar/Sidebar.tsx
+++ src/modules/Sidebar/Sidebar.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image'
 
+import { HiHome, HiGlobe, HiHeart, HiMusicNote } from 'react-icons/hi'
+
 const Sidebar = () => {
   return (
-    <aside className="bg-raisin-black fixed w-56 p-2">
+    <aside className="bg-raisin-black fixed w-56 py-2 px-4">
       <Image
         src={'/images/auditica-logo.svg'}
         alt="Auditica Logo"
@@ -10,7 +12,27 @@ const Sidebar = () => {
         width={100}
         height={35}
       />
-      <h1>hi</h1>
+
+      <nav className="mt-6">
+        <ul className="text-white text-sm">
+          <li className="flex font-bold items-center p-2 bg-red-salsa rounded mb-2">
+            <HiHome className="mr-1 w-4" />
+            <p>Home</p>
+          </li>
+          <li className="flex font-medium items-center p-2 rounded mb-2">
+            <HiGlobe className="mr-1 w-4" />
+            <p>Browse</p>
+          </li>
+          <li className="flex font-medium items-center p-2 rounded mb-2">
+            <HiHeart className="mr-1 w-4" />
+            <p>Favorite</p>
+          </li>
+          <li className="flex font-medium items-center p-2 rounded">
+            <HiMusicNote className="mr-1 w-4" />
+            <p>Library</p>
+          </li>
+        </ul>
+      </nav>
     </aside>
   )
 }
